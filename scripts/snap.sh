85da27b074c04c91bbbcd0b8a8fba572abbb2a06
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2024-12-23 10:46:43 +0300

    synced to llama.cpp tag:b4381 short:b92a14a

---
+++ scripts/snap.sh
@@ -0,0 +1,15 @@
+SHORT=`cd build/llama.cpp && git rev-parse --short HEAD`
+TAG=`cd build/llama.cpp && git tag --points-at HEAD`
+
+# echo "tag:${TAG} short:${SHORT}"
+
+git add --all .
+git commit -m "synced to llama.cpp tag:${TAG} short:${SHORT}"
+git push
+
+git switch main
+git merge dev
+git push
+git switch dev
+
+
