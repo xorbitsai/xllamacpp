9027b51af8b0fb7c07e227c478375259e240ae2d
codingl2k1 <138426806+codingl2k1@users.noreply.github.com>
2025-04-08 12:47:32 +0200

    BLD: HIP build for Linux (#19)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,6 +13,7 @@ build_llamacpp() {
 	PROJECT=${THIRDPARTY}/llama.cpp
 	PREFIX=${CWD}/src/llama.cpp
 	INCLUDE=${PREFIX}/include
+	NPROC=2
 	LIB=${PREFIX}/lib
 	SRC=${PREFIX}/src
 	cd ${PROJECT} && \
@@ -23,20 +24,24 @@ build_llamacpp() {
 		# For tracking changes
 		cp examples/server/server.cpp ${SRC} && \
 		cd build
-  if [[ -z "${XLLAMACPP_BUILD_CUDA}" ]]; then
+  if [[ -n "${XLLAMACPP_BUILD_CUDA}" ]]; then
+	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DGGML_CUDA=ON -DGGML_CUDA_FORCE_MMQ=ON && \
+    cmake --build . --config Release -j ${NPROC} && \
+    cmake --install . --prefix ${PREFIX}
+  elif [[ -n "${XLLAMACPP_BUILD_HIP}" ]]; then
+	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_HIP_COMPILER="$(hipconfig -l)/clang" -DGGML_HIP_ROCWMMA_FATTN=ON -DGGML_HIP=ON && \
+	cmake --build . --config Release -j ${NPROC} && \
+    cmake --install . --prefix ${PREFIX}
+  else
 	if [[ "$(uname -s)" == "Darwin" && "$(uname -m)" == "x86_64" ]]; then
 		cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DGGML_METAL=OFF && \
-		cmake --build . --config Release && \
+		cmake --build . --config Release -j ${NPROC} && \
 		cmake --install . --prefix ${PREFIX}
 	else
 		cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib && \
-		cmake --build . --config Release && \
+		cmake --build . --config Release -j ${NPROC} && \
 		cmake --install . --prefix ${PREFIX}
 	fi
-  else
-    cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DGGML_CUDA=ON -DGGML_CUDA_FORCE_MMQ=ON && \
-    cmake --build . --config Release && \
-    cmake --install . --prefix ${PREFIX}
   fi
   [[ -e common/libcommon.a ]] && cp common/libcommon.a ${LIB}
   [[ -e common/Release/common.lib ]] && cp common/Release/common.lib ${LIB}
