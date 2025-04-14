91be3aba24491bf663a9b112d9bcde73260edea9
codingl2k1 <codingl2k1@outlook.com>
2025-04-14 09:54:29 +0200

    Build with -DLLAMA_CURL=OFF

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -25,20 +25,20 @@ build_llamacpp() {
 		cp examples/server/server.cpp ${SRC} && \
 		cd build
   if [[ -n "${XLLAMACPP_BUILD_CUDA}" ]]; then
-	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DGGML_CUDA=ON -DGGML_CUDA_FORCE_MMQ=ON && \
+	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF -DGGML_CUDA=ON -DGGML_CUDA_FORCE_MMQ=ON && \
     cmake --build . --config Release -j ${NPROC} && \
     cmake --install . --prefix ${PREFIX}
   elif [[ -n "${XLLAMACPP_BUILD_HIP}" ]]; then
-	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_HIP_COMPILER="$(hipconfig -l)/clang" -DGGML_HIP_ROCWMMA_FATTN=ON -DGGML_HIP=ON && \
+	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF -DCMAKE_HIP_COMPILER="$(hipconfig -l)/clang" -DGGML_HIP_ROCWMMA_FATTN=ON -DGGML_HIP=ON && \
 	cmake --build . --config Release -j ${NPROC} && \
     cmake --install . --prefix ${PREFIX}
   else
 	if [[ "$(uname -s)" == "Darwin" && "$(uname -m)" == "x86_64" ]]; then
-		cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DGGML_METAL=OFF && \
+		cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF -DGGML_METAL=OFF && \
 		cmake --build . --config Release -j ${NPROC} && \
 		cmake --install . --prefix ${PREFIX}
 	else
-		cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib && \
+		cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF && \
 		cmake --build . --config Release -j ${NPROC} && \
 		cmake --install . --prefix ${PREFIX}
 	fi
