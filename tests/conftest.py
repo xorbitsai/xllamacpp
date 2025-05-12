6e62ca26e2a4b3e2ae86064c54252f6133efd43c
codingl2k1 <codingl2k1@outlook.com>
2025-05-12 16:43:35 +0200

    Add test for multimodal

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 from pathlib import Path
+
 ROOT = Path(__file__).parent.parent
 
 
 import pytest
 
+
 @pytest.fixture(scope="module")
 def model_path():
-	return str(ROOT / 'models' / 'Llama-3.2-1B-Instruct-Q8_0.gguf')
+    return str(ROOT / "models")
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,6 +1,9 @@
 import json
 import pprint
+import os
 import sys
+import requests
+import base64
 
 print(sys.path)
 import xllamacpp as xlc
@@ -9,7 +12,7 @@ import xllamacpp as xlc
 def test_llama_server(model_path):
     params = xlc.CommonParams()
 
-    params.model.path = model_path
+    params.model.path = os.path.join(model_path, "Llama-3.2-1B-Instruct-Q8_0.gguf")
     params.prompt = "When did the universe begin?"
     params.n_predict = 32
     params.n_ctx = 512
@@ -62,4 +65,54 @@ def test_llama_server(model_path):
     server.handle_metrics(
         lambda s: pprint.pprint(json.loads(s)),
         lambda s: print(s.decode("utf-8")),
+    )
+
+
+def test_llama_server_multimodal(model_path):
+    IMG_URL_0 = (
+        "https://huggingface.co/ggml-org/tinygemma3-GGUF/resolve/main/test/11_truck.png"
+    )
+
+    response = requests.get(IMG_URL_0)
+    response.raise_for_status()  # Raise an exception for bad status codes
+    IMG_BASE64_0 = "data:image/png;base64," + base64.b64encode(response.content).decode(
+        "utf-8"
+    )
+
+    params = xlc.CommonParams()
+
+    params.model.path = os.path.join(model_path, "tinygemma3-Q8_0.gguf")
+    params.mmproj.path = os.path.join(model_path, "mmproj-tinygemma3.gguf")
+    params.sampling.seed = 42
+    params.sampling.top_k = 1
+    params.sampling.temp = 0
+    params.n_predict = 4
+    params.n_ctx = 1024
+    params.cpuparams.n_threads = 4
+    params.cpuparams_batch.n_threads = 2
+
+    server = xlc.Server(params)
+
+    chat_complete_prompt = {
+        "max_tokens": 128,
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "What is this:\n"},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": IMG_BASE64_0,
+                        },
+                    },
+                ],
+            },
+        ],
+    }
+
+    server.handle_chat_completions(
+        json.dumps(chat_complete_prompt),
+        lambda s: pprint.pprint(json.loads(s)),
+        lambda s: pprint.pprint(json.loads(s)),
     )
