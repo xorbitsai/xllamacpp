ef191d2997d5a1e66b8616708b7a437f7b384689
codingl2k1 <codingl2k1@outlook.com>
2025-05-23 12:45:00 +0200

    Update build cflags

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -26,19 +26,30 @@ build_llamacpp() {
 		cp tools/server/server.cpp ${SRC} && \
 		cd build
   if [[ -n "${XLLAMACPP_BUILD_CUDA}" ]]; then
-	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF -DGGML_CUDA=ON -DGGML_CUDA_FORCE_MMQ=ON && \
+  	echo "Building for CUDA"
+	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF -DGGML_BACKEND_DL=ON -DGGML_NATIVE=OFF -DGGML_CPU=OFF -DGGML_CUDA=ON -DGGML_CUDA_FORCE_MMQ=ON && \
     cmake --build . --config Release -j ${NPROC} && \
     cmake --install . --prefix ${PREFIX}
   elif [[ -n "${XLLAMACPP_BUILD_HIP}" ]]; then
-	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF -DCMAKE_HIP_COMPILER="$(hipconfig -l)/clang" -DGGML_HIP_ROCWMMA_FATTN=ON -DGGML_HIP=ON && \
+	echo "Building for AMD GPU"
+	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF -DGGML_BACKEND_DL=ON -DGGML_NATIVE=OFF -DGGML_CPU=OFF -DAMDGPU_TARGETS="gfx1100;gfx1101;gfx1102;gfx1030;gfx1031;gfx1032" -DCMAKE_HIP_COMPILER="$(hipconfig -l)/clang" -DGGML_HIP_ROCWMMA_FATTN=ON -DGGML_HIP=ON && \
 	cmake --build . --config Release -j ${NPROC} && \
     cmake --install . --prefix ${PREFIX}
   else
-	if [[ "$(uname -s)" == "Darwin" && "$(uname -m)" == "x86_64" ]]; then
-		cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF -DGGML_METAL=OFF && \
-		cmake --build . --config Release -j ${NPROC} && \
-		cmake --install . --prefix ${PREFIX}
+	if [[ "$(uname -s)" == "Darwin" ]]; then
+		if [[ "$(uname -m)" == "x86_64" ]]; then
+			echo "Building for Intel"
+			cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_RPATH="@loader_path" -DLLAMA_CURL=OFF -DGGML_METAL=OFF && \
+			cmake --build . --config Release -j ${NPROC} && \
+			cmake --install . --prefix ${PREFIX}
+		else
+			echo "Building for Apple Silicon"
+			cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_RPATH="@loader_path" -DGGML_METAL_USE_BF16=ON -DGGML_METAL_EMBED_LIBRARY=ON -DLLAMA_CURL=OFF && \
+			cmake --build . --config Release -j ${NPROC} && \
+			cmake --install . --prefix ${PREFIX}
+		fi
 	else
+		echo "Building for non-MacOS CPU"
 		cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF && \
 		cmake --build . --config Release -j ${NPROC} && \
 		cmake --install . --prefix ${PREFIX}
