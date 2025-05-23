b20bd5b91d6bcb69bbf1c6944774132fe2f182d6
codingl2k1 <codingl2k1@outlook.com>
2025-05-23 21:59:04 +0200

    -DGGML_NATIVE=OFF for CUDA and HIP

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,11 +18,11 @@ build_llamacpp() {
 		cd build
   if [[ -n "${XLLAMACPP_BUILD_CUDA}" ]]; then
   	echo "Building for CUDA"
-	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF -DGGML_CUDA=ON -DGGML_CUDA_FORCE_MMQ=ON && \
+	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF -DGGML_NATIVE=OFF -DGGML_CUDA=ON -DGGML_CUDA_FORCE_MMQ=ON && \
     cmake --build . --config Release -j ${NPROC} --target common llama ggml ggml-cpu ggml-cuda mtmd_static
   elif [[ -n "${XLLAMACPP_BUILD_HIP}" ]]; then
 	echo "Building for AMD GPU"
-	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF -DAMDGPU_TARGETS="gfx1100;gfx1101;gfx1102;gfx1030;gfx1031;gfx1032" -DCMAKE_HIP_COMPILER="$(hipconfig -l)/clang" -DGGML_HIP_ROCWMMA_FATTN=ON -DGGML_HIP=ON && \
+	cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_LIBDIR=lib -DLLAMA_CURL=OFF -DGGML_NATIVE=OFF -DAMDGPU_TARGETS="gfx1100;gfx1101;gfx1102;gfx1030;gfx1031;gfx1032" -DCMAKE_HIP_COMPILER="$(hipconfig -l)/clang" -DGGML_HIP_ROCWMMA_FATTN=ON -DGGML_HIP=ON && \
 	cmake --build . --config Release -j ${NPROC} --target common llama ggml ggml-cpu ggml-hip mtmd_static
   else
 	if [[ "$(uname -s)" == "Darwin" ]]; then
