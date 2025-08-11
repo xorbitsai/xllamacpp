afbb326edbbeabb5370d1edb4b0c9dce224ecf2a
codingl2k1 <138426806+codingl2k1@users.noreply.github.com>
2025-08-11 13:13:38 +0800

    BLD: Fix CUDA compatibility

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -38,6 +38,7 @@ build_llamacpp() {
       "-DGGML_NATIVE=OFF"
       "-DGGML_CUDA=ON"
       "-DGGML_CUDA_FORCE_MMQ=ON"
+	  "-DCMAKE_CUDA_ARCHITECTURES=all"
     )
     targets+=("ggml-cuda")
   elif [[ -n "${XLLAMACPP_BUILD_HIP}" ]]; then
