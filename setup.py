f332661213d69df854fa7a93abebab7dc6283dce
codingl2k1 <138426806+codingl2k1@users.noreply.github.com>
2025-08-11 13:09:23 +0800

    BUG: pip install -e . may not link libggml-blas.a (#67)

--- setup.py
+++ setup.py
@@ -116,9 +116,10 @@ if PLATFORM == "Darwin":
 elif PLATFORM == "Linux":
     EXTRA_LINK_ARGS.extend(["-fopenmp", "-static-libgcc"])
     # Check if BLAS is enabled in environment
-    if os.environ.get("CMAKE_ARGS", "").find("-DGGML_BLAS=ON") != -1:
+    if os.path.exists(f"{LLAMACPP_LIBS_DIR}/libggml-blas.a"):
         print("BLAS is enabled, adding ggml-blas to link targets")
         EXTRA_OBJECTS.extend([f"{LLAMACPP_LIBS_DIR}/libggml-blas.a"])
+        EXTRA_LINK_ARGS.extend(["-lopenblas"])
 
 INCLUDE_DIRS.append(os.path.join(CWD, "src/xllamacpp"))
 
