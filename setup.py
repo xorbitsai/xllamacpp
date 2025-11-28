3396d21077a42f65a96f5b0bfd3c1b0ccc47e233
codingl2k1 <138426806+codingl2k1@users.noreply.github.com>
2025-11-28 14:27:39 +0800

    Add 'ntdll' to Windows libraries in setup.py

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ LIBRARIES = []
 
 if PLATFORM == "Windows":
     LIBRARIES.extend(
-        ["common", "llama", "ggml", "ggml-base", "ggml-cpu", "mtmd", "cpp-httplib", "llguidance", "Advapi32", "userenv"]
+        ["common", "llama", "ggml", "ggml-base", "ggml-cpu", "mtmd", "cpp-httplib", "llguidance", "Advapi32", "userenv", "ntdll"]
     )
     if BUILD_CUDA:
         LIBRARY_DIRS.extend([os.getenv("CUDA_PATH", "") + "\\Lib\\x64"])
